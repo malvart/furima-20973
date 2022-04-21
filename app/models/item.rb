7535dba1513f7fd71135dc91519ee782385f5216
malvart <102654106+malvart@users.noreply.github.com>
2022-04-21 15:36:33 +0900

    ログインの有無確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :description, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :charge_id, numericality: { other_than: 1, message: "can't be blank" } 
