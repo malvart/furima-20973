9d1f73fe0ad372be0af000bc34fb2c31634b4bf7
malvart <102654106+malvart@users.noreply.github.com>
2022-04-21 17:43:30 +0900

    手数料・利益にカンマ表示

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -4,8 +4,8 @@ priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
   addTax = inputValue / 10;
-  addTaxDom.innerHTML = Math.floor(addTax);
+  addTaxDom.innerHTML = Math.floor(addTax).toLocaleString();
   const profitDom = document.getElementById("profit");
-  profitDom.innerHTML = Math.floor(inputValue - addTax);
+  profitDom.innerHTML = Math.floor(inputValue - addTax).toLocaleString();
 })
 })
