50927faa2eec07a14ef569d1493fb691ddcd4de3
malvart <102654106+malvart@users.noreply.github.com>
2022-04-21 17:37:08 +0900

    手数料、販売利益の自動算出

---
+++ app/javascript/price.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTax = inputValue / 10;
+  addTaxDom.innerHTML = Math.floor(addTax);
+  const profitDom = document.getElementById("profit");
+  profitDom.innerHTML = Math.floor(inputValue - addTax);
+})
+})
