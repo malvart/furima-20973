51ebfa36d31e6a7f02e376cd41ad5d4ff3a4b243
malvart <102654106+malvart@users.noreply.github.com>
2022-04-25 17:27:33 +0900

    payjpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,16 +9,26 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      render "orders/index.html.erb"
+      render :index
     end
   end
 
   private
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name,
-                                          :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+                                          :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipality, :house_number,
-                :building_name, :telephone_number, :user_id, :item_id
+                :building_name, :telephone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class OrderAddress
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
